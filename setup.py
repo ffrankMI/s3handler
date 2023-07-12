73eec03a5421a576ec1576eb92e09a8ca5d8004d
Fabian <fabian.frank@axelspringer.com>
2023-07-12 14:36:49 +0200

    initial commit

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.0.1',
     author='Fabian Frank',
     description='S3 handler for AWS',
-    packages=['s3handler'],
+    packages=find_packages(),
     install_requires=[
         'boto3',
         'pandas'
